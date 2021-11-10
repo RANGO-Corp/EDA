8b51e68b45c332da19a2ec8399ef094d15f45897
ozluuh <ozluuh@gmail.com>
2021-11-10 13:30:53 -0300

    feat(user): add profile view

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -27,6 +27,12 @@ namespace Alere.Controllers
         {
             return View();
         }
+        
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
