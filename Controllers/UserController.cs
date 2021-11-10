aea30ffd120b9d8558454a3651878a4288a962f9
ozluuh <ozluuh@gmail.com>
2021-11-10 19:10:39 -0300

    refactor: add endpoints

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Alere.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -12,6 +13,8 @@ namespace Alere.Controllers
     {
         private readonly ILogger<UserController> _logger;
 
+        private IList<User> _repo = new List<User>();
+
         public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
@@ -19,7 +22,9 @@ namespace Alere.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var users = _repo.ToList();
+
+            return View(users);
         }
 
         [HttpGet]
@@ -27,11 +32,35 @@ namespace Alere.Controllers
         {
             return View();
         }
-        
-        [HttpGet]
-        public IActionResult Profile()
+
+        [HttpPost]
+        public IActionResult Create(User user)
         {
-            return View();
+            _repo.Add(user);
+
+            TempData["msg"] = "Usuário {user.Name} cadastrado com sucesso!";
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Profile(long id)
+        {
+            var user = _repo.Where(u => u.UserId == id);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Profile(User user)
+        {
+            var userIndex = _repo.IndexOf(user);
+
+            _repo.RemoveAt(userIndex);
+
+            _repo.Add(user);
+
+            return RedirectToAction("Profile");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
