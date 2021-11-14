a7d5bd1551012551ded85ff25cc9bd9fb23dc09c
ozluuh <ozluuh@gmail.com>
2021-11-13 23:11:18 -0300

    add login page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace Alere.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
