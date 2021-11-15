093c26b207b1c5e04d15985183fec4cf621012ca
ozluuh <ozluuh@gmail.com>
2021-11-15 09:33:49 -0300

    refactor: add SignUp view with static form

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -17,6 +17,11 @@ namespace Alere.Controllers
             return View();
         }
 
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
