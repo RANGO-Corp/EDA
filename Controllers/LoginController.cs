9ca2ccb948a5a011e5e1b87ac29498dd708de43c
ozluuh <ozluuh@gmail.com>
2021-11-15 08:02:02 -0300

    refactor: change login page to same controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Alere.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
