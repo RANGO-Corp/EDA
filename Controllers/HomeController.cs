8226b0fb40ded94848dc55a6c7db96722704b5c1
ozluuh <ozluuh@gmail.com>
2021-11-17 16:35:14 -0300

    refactor: change authentication to home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using Alere.Helpers;
 using Alere.Models;
+using Alere.Repositories;
+using Alere.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -9,9 +12,12 @@ namespace Alere.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private IUserRepository _repoUser;
+
+        public HomeController(ILogger<HomeController> logger, IUserRepository repoUser)
         {
             _logger = logger;
+            _repoUser = repoUser;
         }
 
         public IActionResult Index()
@@ -19,15 +25,86 @@ namespace Alere.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Login(string error)
+        {
+            if (error != null)
+            {
+                TempData["msg"] = error;
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var user = _repoUser.FindByCondition(c =>
+                c.Email.Equals(loginViewModel.Username) &&
+                c.Password.Equals(loginViewModel.Password)
+            );
+            if (user == null)
+            {
+                return RedirectToAction("Login", new
+                {
+                    error = "Usuário não existente ou credenciais inválidas."
+                });
+            }
+
+            LoadToSession(SessionKeys.USER_KEY.ToString(), user);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            // GET Password from view and SET into User
+            registerViewModel.User.Password = registerViewModel.Password;
+            _repoUser.Store(registerViewModel.User);
+            _repoUser.Commit();
+
+            LoadToSession(SessionKeys.USER_KEY.ToString(), registerViewModel.User);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove(SessionKeys.USER_KEY.ToString());
+
+            return RedirectToAction("Index");
+        }
+
+        private void LoadToSession(string key, object value)
+        {
+            SessionHelper.SetObjectToSession(HttpContext.Session, key, value);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { 
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
         }
     }
 }
